b4d3cd3fb617d2dd94fe0fbbd19d361a13057193
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-09 14:56:21 +0200

    created first project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'white': 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'gray-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)',
+      },
+      fontFamily: {
+        'sans': ['Outfit'],
+      },
+    },
+  },
+  plugins: [],
+}
